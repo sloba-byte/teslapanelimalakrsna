9a59dac9de018067aef540e70b2500fabab900bb
byte_lap <none>
2022-10-26 09:57:11 +0200

    Added tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	//import '../app.postcss';
+</script>
+
+<div class="h-full w-full min-h-screen bg-base-200">
+	<div class="h-full w-full xl:container mx-auto mt-8 px-4">
+		<slot />
+	</div>
+</div>

13a2d32b6b5749bc62a5cfee7828332cc934c2fa
byte_lap <none>
2022-10-28 12:36:41 +0200

    Changes to get to ground zero

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,17 @@
 	<div class="bg-base-100 border-b-2">
 		<div class="navbar xl:container mx-auto bg-base-100 px-4">
 			<div class="navbar-start">
-				<img alt="tesla_paneli_logo.png" src="vizitka.jpg" width="250px" height="150px" />
+				<img
+					alt="Vizit karta"
+					src="vizitka.png"
+					width="250px"
+					height="150px"
+					style="border: 1px solid black"
+				/>
 			</div>
 			<div class="navbar-end space-x-2">
 				<a href="./" class="btn btn-outline">Prodaja</a>
-				<a href="./tesla" class="btn btn-primary btn-outline">Tesla paneli</a>
+				<a href="./tesla" class="btn btn-outline">Tesla paneli</a>
 			</div>
 		</div>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,46 @@
 <h2 class="mt-2 text-center text-3xl font-bold tracking-tight text-base-content">
-	Prodaja tesla sendvič panela Auto Šop Mala Krsna
+	Prodaja tesla sendvič panela
 </h2>
 
-<h3 class="mt-3 text-2xl font-bold tracking-tight text-base-content">Kontakt: 065 80 27 8 27</h3>
-
-<div class="">
-	<iframe
-		title=""
-		width="425"
-		height="350"
-		frameborder="0"
-		scrolling="no"
-		marginheight="0"
-		marginwidth="0"
-		src="https://www.openstreetmap.org/export/embed.html?bbox=21.065903306007385%2C44.6016402857339%2C21.071133613586426%2C44.60420884103771&amp;layer=mapnik"
-		style="border: 1px solid black"
-	/>
-
-	<a
-		rel="noreferrer"
-		class="btn btn-outline btn-primary"
-		target="_blank"
-		href="https://goo.gl/maps/TgrMjtdUaP6hFB859">Nasa lokacija</a
-	>
+<div class="text-center text-xl">
+	<p>Neki text</p>
+	<p>Mozda neka slika</p>
+	<p>Ili sta god da je potrebno dodati, slogan bilo sta</p>
 </div>
+
+<div class="mt-2 text-left text-2xl tracking-tight text-base-content">
+	<a rel="noreferrer" target="_blank" href="https://goo.gl/maps/TgrMjtdUaP6hFB859">
+		<img alt="Nasa lokacija" src="lokacija.png" style="border: 1px solid black" />
+	</a>
+</div>
+
+<footer class="footer p-10 text-xl bg-base-200 text-base-content">
+	<div>
+		<img height="100px" width="150px" alt="Nasa lokacija" src="tesla_paneli_logo.png" />
+		<p>Auto Šop Mala Krsna<br />Prodaja tesla sendvič panela</p>
+	</div>
+	<div>
+		<span class="footer-title">Lokacija</span>
+		<a
+			class="hover:underline"
+			rel="noreferrer"
+			target="_blank"
+			href="https://goo.gl/maps/TgrMjtdUaP6hFB859"
+		>
+			Požarevački put bb <br /> Mala Krsna
+		</a>
+	</div>
+	<div>
+		<span class="footer-title">Telefon</span>
+		<a class="hover:underline" href="tel:0658027827" title="Pozovite nas">065 80 27 8 27</a>
+		<a class="hover:underline" href="tel:026731168" title="Pozovite nas">026 73 11 68</a>
+	</div>
+	<div>
+		<span class="footer-title">E-mail</span>
+		<a class="hover:underline" href="mailto:autosopdoo@yahoo.com">autosopdoo@yahoo.com</a>
+	</div>
+	<div>
+		<span class="footer-title">Radno vreme</span>
+		<p>Pon-Sub: 8-17</p>
+	</div>
+</footer>
